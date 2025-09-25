16ccc96377f50c11a16963e3f4d6a4263e7789be
Alessandro Borsato <75090321+Slthy@users.noreply.github.com>
2025-09-25 15:28:50 -0400

    final commit: polished and commented

--- Enigma.java
+++ Enigma.java
@@ -9,26 +9,26 @@ public class Enigma{
 
     private Rotor rotors[];
         
-    public Enigma(int id1, int id2, int id3, String start){
+    public Enigma(int id1, int id2, int id3, String start){         // public constructor
 
         rotors = new Rotor[3];
-        rotors[0] = new Rotor(rotorInit[id1-1], start.charAt(0));
-        rotors[1] = new Rotor(rotorInit[id2-1], start.charAt(1));
-        rotors[2] = new Rotor(rotorInit[id3-1], start.charAt(2));
+        rotors[0] = new Rotor(rotorInit[id1-1], start.charAt(0));   // inner rotor
+        rotors[1] = new Rotor(rotorInit[id2-1], start.charAt(1));   // middle rotor
+        rotors[2] = new Rotor(rotorInit[id3-1], start.charAt(2));   // outer rotor
         
     }
 
     public String encrypt(String message) {
         String output = "";
 
-        for (char c : message.toCharArray()) {
-            int step_one = rotors[0].indexOf(c);
-            char step_two = rotors[2].charAt(step_one);
-            int step_three = rotors[1].indexOf(step_two);
-            char step_four = rotors[2].charAt(step_three);
-            output += step_four;
+        for (char c : message.toCharArray()) {                      // for every char in the string
+            int step_one = rotors[0].indexOf(c);                    // get the index in the inner rotor
+            char step_two = rotors[2].charAt(step_one);             // inner -> outer
+            int step_three = rotors[1].indexOf(step_two);           // get the index in the middle rotor
+            char step_four = rotors[2].charAt(step_three);          // middle -> outer
+            output += step_four;                                    // update output string
 
-            rotate();
+            rotate();                                               // rotate
         }
         return output;
     }
@@ -37,10 +37,10 @@ public class Enigma{
         String output = "";
 
         for (char c : message.toCharArray()) {
-            int step_one = rotors[2].indexOf(c);
-            char step_two = rotors[1].charAt(step_one);
-            int step_three = rotors[2].indexOf(step_two);
-            char step_four = rotors[0].charAt(step_three);
+            int step_one = rotors[2].indexOf(c);                    // get the index in the outer rotor
+            char step_two = rotors[1].charAt(step_one);             // outer -> middle
+            int step_three = rotors[2].indexOf(step_two);           // get the outer in the outer rotor
+            char step_four = rotors[0].charAt(step_three);          // outer -> inner
             output += step_four;
 
             rotate();
--- Rotor.java
+++ Rotor.java
@@ -1,21 +1,20 @@
 public class Rotor {
 
     private String rotorValues;
-    private final char startChar;
-    
-    public Rotor(String v, char c) {
+    private final char startChar;           // remember with what char I started
+
+    public Rotor(String v, char c) {        // public constructor
 
         this.rotorValues = v;
         this.startChar = c;
 
-        while (rotorValues.charAt(0) != startChar) {
-            rotate();
-        }
+        //rotate till the rotor is properly configurated
+        while (rotorValues.charAt(0) != startChar) rotate();
     }
 
     public boolean rotate() {
-        char last = rotorValues.charAt(rotorValues.length() - 1);
-        rotorValues = last + rotorValues.substring(0, rotorValues.length() - 1);
+        char last = rotorValues.charAt(rotorValues.length() - 1);                   // "123456" -> "6"
+        rotorValues = last + rotorValues.substring(0, rotorValues.length() - 1);    // "6" + "12345"
         return rotorValues.charAt(0) == startChar;
     }
 
